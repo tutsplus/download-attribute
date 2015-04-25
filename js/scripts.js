850d7097450fd340541cdad8ccd78ca0784edb6d
Thoriq Firdaus <tfirdaus@outlook.com>
2015-04-25 11:25:05 +0700

    add fallback in non-supporting browser

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 (function($) {
   'use strict';
 
-  
+  if ( ! Modernizr.adownload ) {
+
+  	var $link = $('a');
+
+  	$link.each(function() {
+  		var $download = $(this).attr('download');
+  	
+  		if (typeof $download !== typeof undefined && $download !== false) {
+        var $el = $('<div>').addClass('download-instruction').text('Right-click and select "Download Linked File"');
+        $el.insertAfter($(this));
+			}
+
+  	});
+
+  }
 
 })(jQuery);
